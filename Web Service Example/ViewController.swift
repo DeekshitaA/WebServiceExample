dc737ef11747c17d444928380934cadc51ea4605
DeekshitaA <deekshita.amaravadi@gmail.com>
2016-02-28 16:41:30 -0500

    Web service call using AFNetworking

--- Web Service Example/ViewController.swift
+++ Web Service Example/ViewController.swift
@@ -8,11 +8,46 @@
 
 import UIKit
 
+import SwiftyJSON
+
+import LTMorphingLabel
+
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var forecastLabel: LTMorphingLabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.forecastLabel.text = ""
+        
+        //instantiate a gray Activity Indicator View
+        let activityIndicatorView = UIActivityIndicatorView(activityIndicatorStyle: .Gray)
+        //add the activity to the ViewController's view
+        view.addSubview(activityIndicatorView)
+        //position the Activity Indicator View in the center of the view
+        activityIndicatorView.center = view.center
+        //tell the Activity Indicator View to begin animating
+        activityIndicatorView.startAnimating()
+        
+        let manager = AFHTTPSessionManager()
+        
+        manager.GET("http://api.openweathermap.org/data/2.5/forecast/daily?q=London&mode=json&units=metric&cnt=1&appid=2f3c40ad7ab3b9d444c8eb7b05691f22",
+            parameters: nil,
+            progress: nil,
+            success: { (operation,responseObject) in
+            
+                let json = JSON(responseObject!)
+                if let forecast = json["list"][0]["weather"][0]["description"].string {
+                    self.forecastLabel.text = forecast
+                }
+                activityIndicatorView.removeFromSuperview()
+                
+            },
+            failure: { (operation,error) in
+                print("Error: " + error.localizedDescription)
+        })
     }
 
     override func didReceiveMemoryWarning() {
---
+++ bridge.h
@@ -0,0 +1,14 @@
+//
+//  bridge.h
+//  Web Service Example
+//
+//  Created by Deekshita Amaravadi on 2/14/16.
+//  Copyright © 2016 Deekshita Amaravadi. All rights reserved.
+//
+#import "AFNetworking/AFNetworking.h"
+
+#ifndef bridge_h
+#define bridge_h
+
+
+#endif /* bridge_h */
